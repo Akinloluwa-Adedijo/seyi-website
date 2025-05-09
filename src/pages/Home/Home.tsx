959e08c81ef25445a18a6e8eadeff5b1158157a8
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2025-05-09 11:41:44 -0700

    landing page setup

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Home = () => {
-  return <div>this is the home page</div>;
+  return (
+    <main className="relative flex min-h-screen mb-100 overflow-hidden">
+      <img src="" alt="Ṣèyí,ThePoet Profile Image" loading="lazy" />
+
+      {/* slider container */}
+      <div className="absolute top-10">
+        {/* slider */}
+        <div>
+          <h1 className="text-7xl font-bold uppercase">Sèyí,ThePoet</h1>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
