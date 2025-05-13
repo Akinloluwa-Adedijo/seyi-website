dd688c72afea7282c75b82a905edbb2b73e56c43
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2025-05-13 16:01:53 -0700

    review modal text scroll fix

--- src/components/ReviewModal/ReviewModal.tsx
+++ src/components/ReviewModal/ReviewModal.tsx
@@ -26,25 +26,51 @@ const ReviewModal = ({ isOpen, onClose, item }: ReviewModalProps) => {
     return null;
   }
   return (
-    <section className="fixed inset-0 z-50 flex items-center justify-center">
+    <section className="fixed inset-0 z-50 flex items-center justify-center p-5">
       {/* backdrop */}
-      <div
-        className="absolute inset-0 bg-black-950 backdrop-blur-3xl opacity-30"
-        onClick={onClose}
-      />
+      <div className="absolute inset-0 backdrop-blur-sm " onClick={onClose} />
 
       {/* modal */}
-      <div className="relative bg-yellow-300 border border-amber-50 rounded-lg w-full max-w-3xl max-h-[80vh] overflow-auto z-10 p-5">
-        <button onClick={onClose} aria-label="Close Modal">
+      <div className="relative bg-yellow-300  rounded-lg w-full max-w-2xl max-h-[80vh] overflow-auto z-10 p-5 text-black">
+        <button
+          onClick={onClose}
+          aria-label="Close Modal"
+          className="text-xl uppercase font-semibold cursor-pointer"
+        >
           Close
         </button>
 
         {/* content */}
-        <div>
-          <p>{item.album_name}</p>
+        <div className="py-10">
           <div>
-            <p>{item.artist}</p>
-            <p>{item.released}</p>
+            <p className="text-4xl uppercase font-black py-2">
+              {item.album_name}
+            </p>
+            <div className="flex justify-between text-xl font-medium">
+              <p>{item.artist}</p>
+              <p>{item.released}</p>
+            </div>
+          </div>
+
+          <div>
+            {item.type === "music" ? (
+              <div>
+                {item.content.map((it, index) => {
+                  return (
+                    <div key={index}>
+                      <div>
+                        <img src={it.src} alt="alt text goes here" />
+                      </div>
+                      <div>{it.text}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            ) : (
+              <>
+                <p>this is an item</p>
+              </>
+            )}
           </div>
         </div>
       </div>
